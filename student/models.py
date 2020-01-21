05de7eada5963913676d08673c1dd1b5c70be861
theharishjangid <harish.n.jangid@gmail.com>
2020-01-21 16:21:10 +0530

    school management system, fourth commit

--- student/models.py
+++ student/models.py
@@ -7,11 +7,11 @@ class Student(models.Model):
     date_of_birth = models.DateField(verbose_name='DOB')
     email = models.EmailField()
     phone_no = models.IntegerField()
+    classroom = models.ForeignKey('ClassRoom', on_delete=models.CASCADE, default=None)
 
 class ClassRoom(models.Model):
     room_no = models.IntegerField()
-    Subject = models.TextField(max_length=50)
-    staff = models.ForeignKey('Staff', on_delete=models.CASCADE, default=None)
+    staff = models.ManyToManyField('Staff', default=None)
 
 class Staff(models.Model):
     first_name = models.TextField(verbose_name='First name', max_length=20)
@@ -22,9 +22,9 @@ class Staff(models.Model):
 
 class Subject(models.Model):
     name = models.TextField(verbose_name='subject name', max_length=20)
-    staff = models.OneToOneField('Staff', on_delete=models.CASCADE, default=None)
+    staff = models.ForeignKey('Staff', on_delete=models.CASCADE, default=None)
 
 class Marks(models.Model):
-    subject = models.ManyToManyField('Subject')
+    subject = models.ForeignKey('Subject',on_delete=models.CASCADE, default=None)
     student = models.ForeignKey('Student',on_delete=models.CASCADE, default=None)
     marks = models.IntegerField()
